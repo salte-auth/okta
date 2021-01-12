77ee38eeb0a2df286a19a0b98558c4b63328fcb9
Cecilia Woodward <ceci@salte.io>
2021-01-11 19:14:58 -0600

    chore: bump dependencies

--- src/okta.ts
+++ src/okta.ts
@@ -5,15 +5,15 @@ export class Okta extends OpenIDProvider {
     super(config);
   }
 
-  public get name() {
+  public get name(): string {
     return 'okta';
   }
 
-  public get login() {
+  public get login(): string {
     return this.url(`${this.config.url}/oauth2/v1/authorize`);
   }
 
-  public get logout() {
+  public get logout(): string {
     return this.url(`${this.config.url}/oauth2/v1/logout`, {
       id_token_hint: this.idToken.raw,
       post_logout_redirect_uri: this.redirectUrl('logout')
